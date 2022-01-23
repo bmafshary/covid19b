eaa01e7f4275fb72d6c21a596ec046b0d6df50f3
bmafshary <67156341+bmafshary@users.noreply.github.com>
2022-01-23 19:18:05 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,16 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 # https://www.kaggle.com/tawsifurrahman/covid19-radiography-database
-# import cv2
-# import matplotlib.image as mpimg
 import random
 from keras.models import load_model
 import numpy as np
-import matplotlib.image as mpimg
-
+from PIL import Image
 inputmodel = load_model('cov200.h5')
 random.seed()
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static'
-
 databasefolder = 'static'
-
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -26,22 +21,15 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     txt = ''
-    filename = 0
     completeadd = ""
-    extrafile = ''
+    b=""
+    p=0
     if request.method == 'POST':
         file = request.files["file"]
         whichone = request.form["radiob"]
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
         filelist1 = os.listdir(databasefolder)
-        # extrafile = os.listdir(databasefolder)
-        '''for i in range(1,11,1):
-            aaaa="Sample_NYILAI("+str(i)+").png"
-            extrafile.remove(aaaa)
-        ''''''for filename in extrafile:
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        '''
 
         if whichone == "rand":
             rand_image = random.choice(filelist1)
@@ -49,21 +37,17 @@ def upload_file():
         if whichone == "upload":
             completeadd = os.path.join(app.config['UPLOAD_FOLDER'], "save.fil")
             file.save(completeadd)
-            #        fileimage = cv2.imread(completeadd, 1)
-        fileimage = mpimg.imread(completeadd)
-        #         fileimage = mpimg.imread('g4g.png')
-        #image1 = fileimage.resize((260, 260)).reshape(1, 260, 260, 3)
-        #image1 = fileimage.resize((260, 260)).reshape(1, 260, 260, 3)
-        # b = inputmodel.predict(image1)
+        image1 = Image.open(completeadd)
+        image2 = image1.resize((260, 260))
+        image3=np.zeros((1,260,260,3))
+        image3[0,:,:,0]=image2
+        b = inputmodel.predict(image3)
+        ind = np.argmax(b[0][:])
+        p=round(b[0][ind],1)
 
-        # ind = np.argmax(b[0][:])
-        # p=round(b[0][ind]*100,1)
-        ind = 1
-        p = 13
         predtex = ["COVID 19 symptom", "Lung Opacity symptom", " normal lung", "Viral Pneumonia symptom"]
-        txt = "Based on the modeling, X-Ray images belongs to a patient who has " + predtex[
-            ind] + " by the probability of " + str(p) + " percent"
-    return render_template("index.html", outputtext=txt, image=completeadd, x="")
+        txt = "Based on the modeling, X-Ray images belongs to a patient who has " + predtex[ind] + " by the probability of " + str(p) + " percent"
+    return render_template("index.html", outputtext=txt, image=completeadd,x=b)
 
 
 if __name__ == '__main__':
